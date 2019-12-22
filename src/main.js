a1ed53bf58e9849472699a6cfb2e733dafc33235
lixin <1254759600@qq.com>
2019-12-22 21:40:45 +0800

    init my project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './plugins/element.js'
+import './assets/css/global.css'
+import Axios from 'axios'
+// 配置请求根路径
+Axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
+Vue.prototype.$http = Axios
 
 new Vue({
   router,
